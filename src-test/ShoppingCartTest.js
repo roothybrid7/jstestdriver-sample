94af1cb7a1e76dc12097dd47205bfb633dab88e1
Satoshi Ohki <roothybrid7@gmail.com>
2011-11-27 12:41:18 +0900

    Modified test case function to inline

--- src-test/ShoppingCartTest.js
+++ src-test/ShoppingCartTest.js
@@ -1,12 +1,23 @@
 /**
  * ShoppingCartTest.js - Test case of ShoppingCart.
  */
-ShoppingCartTest = TestCase('ShoppingCartTest');
+//ShoppingCartTest = TestCase('ShoppingCartTest');
+//
+//ShoppingCartTest.prototype.testAdd = function() {
+//  var cart = new shop.Cart();
+//  cart.add('hoge', 10);
+//  cart.add('hoge', 5);
+//  assertEquals(15, cart.getAmount('hoge'));
+//  jstestdriver.console.log('cart.getAmount("hoge"): ', cart.getAmount('hoge'));
+//};
 
-ShoppingCartTest.prototype.testAdd = function() {
-  var cart = new shop.Cart();
-  cart.add('hoge', 10);
-  cart.add('hoge', 5);
-  assertEquals(15, cart.getAmount('hoge'));
-  jstestdriver.console.log('cart.getAmount("hoge"): ', cart.getAmount('hoge'));
-};
+TestCase('ShoppingCartTest', {
+  // inline code here.
+  testAdd: function() {
+    var cart = new shop.Cart();
+    cart.add('hoge', 10);
+    cart.add('hoge', 5);
+    assertEquals(15, cart.getAmount('hoge'));
+    jstestdriver.console.log('cart.getAmount("hoge"): ', cart.getAmount('hoge'));
+  }
+});
