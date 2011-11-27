4cda6b3974f92ef12421770f35ffc93bb74633ac
Satoshi Ohki <roothybrid7@gmail.com>
2011-11-27 12:51:57 +0900

    add rest case

--- src-test/ShoppingCartTest.js
+++ src-test/ShoppingCartTest.js
@@ -1,23 +1,42 @@
 /**
  * ShoppingCartTest.js - Test case of ShoppingCart.
  */
-//ShoppingCartTest = TestCase('ShoppingCartTest');
-//
-//ShoppingCartTest.prototype.testAdd = function() {
-//  var cart = new shop.Cart();
-//  cart.add('hoge', 10);
-//  cart.add('hoge', 5);
-//  assertEquals(15, cart.getAmount('hoge'));
-//  jstestdriver.console.log('cart.getAmount("hoge"): ', cart.getAmount('hoge'));
-//};
+ShoppingCartTest = TestCase('ShoppingCartTest');
 
-TestCase('ShoppingCartTest', {
-  // inline code here.
-  testAdd: function() {
-    var cart = new shop.Cart();
-    cart.add('hoge', 10);
-    cart.add('hoge', 5);
-    assertEquals(15, cart.getAmount('hoge'));
-    jstestdriver.console.log('cart.getAmount("hoge"): ', cart.getAmount('hoge'));
-  }
-});
+ShoppingCartTest.prototype.testAdd = function() {
+  var cart = new shop.Cart();
+  cart.add('hoge', 10);
+  cart.add('hoge', 5);
+  assertEquals(15, cart.getAmount('hoge'));
+  jstestdriver.console.log('cart.getAmount("hoge"): ', cart.getAmount('hoge'));
+};
+
+ShoppingCartTest.prototype.testRemove = function() {
+  var cart = new shop.Cart();
+  cart.add('piyo', 20);
+  cart.remove('piyo', 10);
+  assertEquals(10, cart.getAmount('piyo'));
+  jstestdriver.console.log('cart.getAmount("piyo"): ', cart.getAmount('piyo'));
+};
+
+ShoppingCartTest.prototype.testClear = function() {
+  var cart = new shop.Cart();
+  cart.add('hoge', 10);
+  cart.add('piyo', 20);
+  cart.clear();
+  assertEquals(0, cart.getAmount('hoge'));
+  assertEquals(0, cart.getAmount('piyo'));
+  jstestdriver.console.log('cart.getAmount("hoge"): ', cart.getAmount('hoge'));
+  jstestdriver.console.log('cart.getAmount("piyo"): ', cart.getAmount('piyo'));
+};
+
+//TestCase('ShoppingCartTest', {
+//  // inline code here.
+//  testAdd: function() {
+//    var cart = new shop.Cart();
+//    cart.add('hoge', 10);
+//    cart.add('hoge', 5);
+//    assertEquals(15, cart.getAmount('hoge'));
+//    jstestdriver.console.log('cart.getAmount("hoge"): ', cart.getAmount('hoge'));
+//  }
+//});
